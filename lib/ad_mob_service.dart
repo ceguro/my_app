f44b8adfc2bef1c79b1e16a56a7c98dd083f3009
Carlos Gutiérrez <ceguro10@gmail.com>
2023-06-25 01:06:30 -0300

    fix rewarded ad

---
+++ lib/ad_mob_service.dart
@@ -0,0 +1,12 @@
+
+class AdMobService {
+  static String? get rewardedAdUnitId {
+    if (true) {
+      return 'ca-app-pub-3940256099942544/5224354917';
+    } else {
+      return 'ca-app-pub-3940256099942544/5224354917';
+    }
+    return null;
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:my_app/ad_mob_service.dart';
 
 void main() {
-
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
   runApp(MyApp());
@@ -15,102 +13,90 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    // Initialize the MobileAds SDK.
-    MobileAds.instance.initialize();
-
-    // Create a banner ad.
-    BannerAd ad = BannerAd(
-      adUnitId: "ca-app-pub-2516074192821779/4693565384",
-      size: AdSize.banner,
-      request: AdRequest(),
-      listener: Banner
-    );
-
-    // Add the ad to the app.
-    ad.load();
-
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'My App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+    return MaterialApp(
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.black),
         ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    return Scaffold(
-      body: Column(
-        children: [
-          Text('An awesome recipe:'),
-          BigCard(pair: pair),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Show Ads'),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            textStyle: const TextStyle(fontSize: 20),
+            minimumSize: const Size(200, 50),
           ),
-        ],
+        ),
       ),
+      home: const HomePage(),
     );
   }
 }
 
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  final WordPair pair;
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  RewardedAd? _rewardedAd;
+
+  @override
+  void initState() {
+    super.initState();
+    _createRewardedAd();
+  }
+
+  void _createRewardedAd() {
+    RewardedAd.load(
+      adUnitId: AdMobService.rewardedAdUnitId!,
+      request: const AdRequest(),
+      rewardedAdLoadCallback: RewardedAdLoadCallback(
+        onAdLoaded: (ad) => setState(() => _rewardedAd = ad),
+        onAdFailedToLoad: (error) => setState(() => _rewardedAd = null),
+      ),
+    );
+  }
+
+  void _showRewardedAd() {
+    if (_rewardedAd != null) {
+      _rewardedAd!.fullScreenContentCallback = FullScreenContentCallback(
+        onAdDismissedFullScreenContent: (ad) {
+          ad.dispose();
+          _createRewardedAd();
+        },
+        onAdShowedFullScreenContent: (ad) {
+          ad.dispose();
+          _createRewardedAd();
+        },
+      );
+      _rewardedAd!.show(
+        onUserEarnedReward: (ad, reward) {
+          print('User earned reward: ${reward.amount}.');
+        },
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-    
-    return Card(
-      color: theme.colorScheme.primary,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase, style: style,),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rewarded App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Spacer(),
+            ElevatedButton(
+                onPressed: _showRewardedAd,
+                child: const Text('Show Rewarded Ad')),
+            const Spacer(),
+          ],
+        ),
       ),
     );
-  }
-}
-
-class BannerAdListener extends AdListener {
-  @override
-  void onAdLoaded() {
-    // Called when an ad is successfully loaded.
-    print('Ad loaded.');
-  }
-
-  @override
-  void onAdFailedToLoad(LoadAdError error) {
-    // Called when an ad request failed.
-    print('Ad failed to load: $error');
   }
 }
